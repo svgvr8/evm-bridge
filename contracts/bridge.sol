6ddde45a4c124590f37966f1292cb78263cfbb63
Sagar <41965306+svgvr8@users.noreply.github.com>
2022-04-13 14:26:35 +0530

    bridge.sol vison statement :heavy_division_sign:

--- contracts/bridge.sol
+++ contracts/bridge.sol
@@ -3,3 +3,8 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+
+/** What should this code do? 
+- 1. Lock the token & supplies
+- 2. Unlock the token supply from an authoirized member after cross-chain migration
+ */
