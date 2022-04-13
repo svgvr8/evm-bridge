c33997818c4e5db1bd22fcc27811b93e7d6f055e
Sagar <41965306+svgvr8@users.noreply.github.com>
2022-04-13 14:12:55 +0530

    npm i @openzeppelin/contracts :1234:

--- contracts/bridge.sol
+++ contracts/bridge.sol
@@ -1,2 +1,5 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
